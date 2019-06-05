15a1f4bc87ad404bc051d992a1be0150c28a4732
Anderson Rolf <email.the.anderson@gmail.com>
2019-06-05 18:44:07 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,19 +1,31 @@
 def addition(num1, num2)
+  val = num1 + num2
+  val
 end
 
 def subtraction(num1, num2)
+  val = num1 - num2
+  val
 end
 
 def division(num1, num2)
+  val = num1/num2
+  val
 end
 
 def multiplication(num1, num2)
+  val = num1*num2
+  val
 end
 
 def modulo(num1, num2)
+  val = num1 % num2
+  val
 end
 
 def square_root(num)
+  val = Math.sqrt(num)
+  val
 end
 
 def order_of_operation(num1, num2, num3, num4)
